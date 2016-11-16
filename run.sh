5b66f4a767e0f57fe35d7d202fae40fdde38195f
Igor Pavlov <nwlunatic@yandex.ru>
2016-11-17 00:04:15 +0700

    add run.sh helper

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+UDID=$(system_profiler SPUSBDataType | sed -n -E -e '/(iPhone|iPad)/,/Serial/s/ *Serial Number: *(.+)/\1/p')
+echo $UDID
+PORT=12345
+RESOLUTION="400x600"
+
+./build/ios_minicap \
+    --udid $UDID \
+    --port $PORT \
+    --resolution $RESOLUTION
