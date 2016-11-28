19ffa34e0e2b59aab78f7890e9fa7b94915aaae3
Simo Kinnunen <simo@shoqolate.com>
2016-11-28 14:50:05 +0900

    Use the new minicap module for parsing the stream. Flow control still needed.

--- example/app.js
+++ example/app.js
@@ -1,175 +1,77 @@
-var http = require('http')
-var path = require('path')
-var net = require('net')
+const http = require('http')
+const path = require('path')
+const net = require('net')
 
-var express = require('express')
-var WebSocketServer = require('ws').Server
-var debug = require('debug')('example')
+const express = require('express')
+const WebSocketServer = require('ws').Server
+const debug = require('debug')('example')
+const { BannerParser, FrameParser } = require('minicap')
 
-var app = express()
+const app = express()
 
-var PORT = process.env.PORT || 9002
+const PORT = process.env.PORT || 9002
 
 app.use(express.static(path.join(__dirname, '/public')))
 
-var server = http.createServer(app)
-var wss = new WebSocketServer({ server: server })
+const server = http.createServer(app)
+const wss = new WebSocketServer({ server: server })
 
-wss.on('connection', function(ws) {
+wss.on('connection', (ws) => {
   console.info('Got a client')
 
-  var stream = net.connect({
+  const stream = net.connect({
     port: 12345
   })
 
-  stream.on('error', function(err) {
+  stream.on('error', (err) => {
     console.error(err)
     console.error('Be sure to run `ios-minicap --port 12345`')
     process.exit(1)
   })
 
-  var readBannerBytes = 0
-  var bannerLength = 2
-  var readFrameBytes = 0
-  var frameBodyLength = 0
-  var frameBody = new Buffer(0)
-  var banner = {
-    version: 0
-  , length: 0
-  , pid: 0
-  , realWidth: 0
-  , realHeight: 0
-  , virtualWidth: 0
-  , virtualHeight: 0
-  , orientation: 0
-  , quirks: 0
+  let bannerParsed = false
+  const bannerParser = new BannerParser()
+  const frameParser = new FrameParser()
+
+  function onBannerAvailable (banner) {
+    debug('banner', banner)
   }
 
-  function tryRead() {
-    for (var chunk; (chunk = stream.read());) {
-      debug('chunk(length=%d)', chunk.length)
-      for (var cursor = 0, len = chunk.length; cursor < len;) {
-        if (readBannerBytes < bannerLength) {
-          switch (readBannerBytes) {
-          case 0:
-            // version
-            banner.version = chunk[cursor]
-            break
-          case 1:
-            // length
-            banner.length = bannerLength = chunk[cursor]
-            break
-          case 2:
-          case 3:
-          case 4:
-          case 5:
-            // pid
-            banner.pid +=
-              (chunk[cursor] << ((readBannerBytes - 2) * 8)) >>> 0
-            break
-          case 6:
-          case 7:
-          case 8:
-          case 9:
-            // real width
-            banner.realWidth +=
-              (chunk[cursor] << ((readBannerBytes - 6) * 8)) >>> 0
-            break
-          case 10:
-          case 11:
-          case 12:
-          case 13:
-            // real height
-            banner.realHeight +=
-              (chunk[cursor] << ((readBannerBytes - 10) * 8)) >>> 0
-            break
-          case 14:
-          case 15:
-          case 16:
-          case 17:
-            // virtual width
-            banner.virtualWidth +=
-              (chunk[cursor] << ((readBannerBytes - 14) * 8)) >>> 0
-            break
-          case 18:
-          case 19:
-          case 20:
-          case 21:
-            // virtual height
-            banner.virtualHeight +=
-              (chunk[cursor] << ((readBannerBytes - 18) * 8)) >>> 0
-            break
-          case 22:
-            // orientation
-            banner.orientation += chunk[cursor] * 90
-            break
-          case 23:
-            // quirks
-            banner.quirks = chunk[cursor]
-            break
-          }
+  function onFrameAvailable (frame) {
+    ws.send(frame.buffer, {
+      binary: true
+    })
+  }
 
-          cursor += 1
-          readBannerBytes += 1
-
-          if (readBannerBytes === bannerLength) {
-            debug('banner', banner)
+  function tryParse () {
+    for (let chunk; (chunk = stream.read());) {
+      do {
+        if (!bannerParsed) {
+          const result = bannerParser.parse(chunk)
+          if (result.state === BannerParser.COMPLETE) {
+            bannerParsed = true
+            onBannerAvailable(result.take())
           }
+          chunk = result.rest
+        } else {
+          const result = frameParser.parse(chunk)
+          if (result.state === FrameParser.COMPLETE) {
+            onFrameAvailable(result.take())
+          }
+          chunk = result.rest
         }
-        else if (readFrameBytes < 4) {
-          frameBodyLength += (chunk[cursor] << (readFrameBytes * 8)) >>> 0
-          cursor += 1
-          readFrameBytes += 1
-          debug('headerbyte%d(val=%d)', readFrameBytes, frameBodyLength)
-        }
-        else {
-          if (len - cursor >= frameBodyLength) {
-            debug('bodyfin(len=%d,cursor=%d)', frameBodyLength, cursor)
-
-            frameBody = Buffer.concat([
-              frameBody
-            , chunk.slice(cursor, cursor + frameBodyLength)
-            ])
-
-            // Sanity check for JPG header, only here for debugging purposes.
-            if (frameBody[0] !== 0xFF || frameBody[1] !== 0xD8) {
-              console.error(
-                'Frame body does not start with JPG header', frameBody)
-              process.exit(1)
-            }
-
-            ws.send(frameBody, {
-              binary: true
-            })
-
-            cursor += frameBodyLength
-            frameBodyLength = readFrameBytes = 0
-            frameBody = new Buffer(0)
-          }
-          else {
-            debug('body(len=%d)', len - cursor)
-
-            frameBody = Buffer.concat([
-              frameBody
-            , chunk.slice(cursor, len)
-            ])
-
-            frameBodyLength -= len - cursor
-            readFrameBytes += len - cursor
-            cursor = len
-          }
-        }
-      }
+      } while (chunk.length)
     }
   }
 
-  stream.on('readable', tryRead)
+  stream.on('readable', tryParse)
+  tryParse()
 
-  ws.on('close', function() {
+  ws.on('close', () => {
     console.info('Lost a client')
     stream.end()
   })
 })
 
 server.listen(PORT)
-console.info('Listening on port %d', PORT)
+console.info(`Listening on port ${PORT}`)
